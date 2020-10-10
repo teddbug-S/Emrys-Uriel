489834b338fea07f836594baea5ee7b2979d72e1
Lukasz Kwiecinski <lukkwie1811@gmail.com>
2020-10-10 12:59:46 +0100

    Added feature to search in a specific path for a file & cleaned up the code

--- CLI/search/__main__.py
+++ CLI/search/__main__.py
@@ -2,40 +2,78 @@
 import os
 import sys
 
-def main():
+def search_extension(path, ext):
+    """Searches for extensions in the path"""
     output = []
+    for root, dirs, files in os.walk(path, topdown=True):
+        for file in files:
+            if file.endswith(ext):
+                path = os.path.join(root, file)
+                output.append(path)
+
+    return output
+
+
+def search_file(path, f):
+    """Searches for files in the path"""
+    output = []
+    for root, dirs, files in os.walk(path, topdown=True):
+        for file in files:
+            if file == f:
+                path = os.path.join(root, file)
+                output.append(path)
+
+    return output
+
+
+def main():
     # arguments passed in the command line
     args = sys.argv[1:]
-    # the directory where we are executing the command
-    dir = os.getcwd()
-    # thanks to this, we ensure that there is only one argument passed
+    # if there's only one argument passed, the directory is goint to be the cwd
     if len(args) == 1:
-        arg = args[0]
+        # the directory where we are executing the command
+        dir = os.getcwd()
         # to know if the argument is an extension or a file
-        if arg.startswith('.'):
-            # is an extension
+        if args[0].startswith('.'):
             ext = arg
-            for root, dirs, files in os.walk(dir, topdown=True):
-                for _file in files:
-                    if _file.endswith(ext):
-                        path = os.path.join(root, _file)
-                        output.append(path)
+            output = search_extension(dir, ext)
+
         else:
-            # is a file
-            file = arg
-            for root, dirs, files in os.walk(dir, topdown=True):
-                for _file in files:
-                    if _file == file:
-                        path = os.path.join(root, _file)
-                        output.append(path)
+            file = args[0]
+            output = search_file(dir, file)
 
         if len(output) == 0:
-            print("Couldn't find that file")
+            print("Couldn't find that file.")
+
         else:
             print(f'Found {len(output)} matches.\n')
             [print(x) for x in output]
+
+    # is there's two arguments passed, the first one is the directory to search, and the other one is the file/ext
+    elif len(args) == 2:
+        dir = args[0]
+        arg = args[1]
+        # checking if the directory exists
+        if os.path.exists(dir):
+            # checking if it's an extension or file
+            if arg.startswith('.'):
+                output = search_extension(dir, arg)
+
+            else:
+                output = search_file(dir, arg)
+
+            if len(output) == 0:
+                print("Couldn't find that file.")
+
+            else:
+                print(f'Found {len(output)} matches.\n')
+                [print(x) for x in output]
+
+        else:
+            print("Directory does not exist.")
+
     else:
-        print('The syntax of the command is incorrect')
+        print('The syntax of the command is incorrect.')
 
 
 
--- CLI/setup.py
+++ CLI/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='CLI',
-    version='0.1.1',
+    version='0.1.2',
     packages=['search'],
     entry_points={
         'console_scripts': [
