27e75619e3229a31b225c281921413b1b6ae0e29
Lukasz Kwiecinski <lukkwie1811@gmail.com>
2020-10-10 12:00:06 +0100

    Error while installing the package fixed

--- CLI/setup.py
+++ CLI/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='CLI',
     version='0.1.0',
-    packages='search',
+    packages=['search'],
     entry_points={
         'console_scripts': [
             'search = search.__main__:main'
