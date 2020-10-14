e87953587fe197e9e1b504ae48e2af4c93391779
Divine Darkey <teddbug47@outlook.com>
2020-10-14 15:25:31 +0100

    Implemented python regex for more powerful searching

--- CLI/searchword/__main__.py
+++ CLI/searchword/__main__.py
@@ -1,50 +1,82 @@
-import os
-from colored import fg, attr, bg
+import re
+from colored import bg, fg, attr
 
 
-# asking for file we'll be working with
-def ask_file():
-    user_input = input(r'File: ')
-    return user_input
+# asking for file
+file = input(r"Enter file path: ")
 
-# takes search query and file content
-def do_search(query, con):
-    # for veryfying search result
+try:  # try opening and reading file
+    with open(file, 'r') as f:
+        contents = f.read()
+except FileNotFoundError:
+    print('File not found')
+
+# defining a search pattern for names
+names_pattern = re.compile(r'(Mr|Miss).+?\w+', re.I)
+# using the search pattern on contents of the file
+name_matches = names_pattern.finditer(contents)
+# gathering results
+names = [name.group() for name in name_matches]
+# finally printing them
+if names:
+    print('\nNames:')
+    for name in names:
+        print(f"-- {name}")
+
+# same process for telephone numbers
+telephone_pattern = re.compile(r'\+\d+.+?\d+.+?\d{3}.+?(\d{4}|\d{3})', re.I)
+telephone_matches = telephone_pattern.finditer(contents)
+telephones = [numbers.group() for numbers in telephone_matches]
+if telephones:
+    print('\nTelephone Numbers:')
+    for number in telephones:
+        print(f"-- {number}")
+
+# same process for emails
+emails_pattern = re.compile(r'\w+.?@\w+\.(com|net|org|mail)', re.I)
+email_matches = emails_pattern.finditer(contents)
+email_addresses = [email.group() for email in email_matches]
+if email_addresses:
+    print('\nEmail Addresses:')
+    for address in email_addresses:
+        print(f"-- {address}")
+
+# for web addresses
+url_patterns = re.compile(r'http(s)?://(w{3})?\.?\w+\.(com|net|org|mail)', re.I)
+url_matches = url_patterns.finditer(contents)
+urls = [url.group() for url in url_matches]
+if urls:
+    print('\nWeb Addresses:')
+    for url in urls:
+        print(f"-- {url}")
+
+
+# now defining a function to all user to do a custom search
+def do_search(query):
+    # parsing the query entered by user into a regex pattern
+    search_pattern = re.compile(fr"{query}", re.I)
+    # for confirming matches
     found = False
-    # if search in the file content
+    # splitting contents from file into individual lines
+    con = contents.split('\n')
+    # if user searches for a line
     if query in con:
-        # getting the position
         position = con.index(query) + 1
-        # printing it
         print(f'{query} found at line {position}.')
     else:
-        # getting each line
         for line in con:
-            # else if query in a line maybe a word
-            if query in (listed_line := line.split()):
-                # getting word in line position
-                word_pos = listed_line.index(query)
-                # getting line position in content or file
-                line_pos = con.index(line)+1
-                # changing or coloring the found word in the line
-                listed_line[word_pos] = "%s%s{}%s".format(query) % (bg(226), fg(232), attr('reset'))
-                # finally printing it
-                print(f"\nWord found at {word_pos + 1} in line {line_pos}.")
-                print("'{}'".format(" ".join(listed_line)))
+            search_matches = search_pattern.search(line)
+            if search_matches:
+                start_pos = search_matches.start()
+                end_pos = search_matches.end()
+                line_pos = con.index(line)
+                print(f"\nWord found at {start_pos+1} in line {line_pos}.")
+                print(line[:start_pos]+f"%s%s{line[start_pos:end_pos]}%s" % (fg(232), bg(226), attr('reset'))+line[end_pos:])
                 found = True
     if not found:
         print('No matches found')
 
 
-def main():
-    file_path = ask_file()
-    while True:
-        quest = input('\nSearch line or word: ').strip()
-        with open(file_path, 'r') as f:
-            contents = f.read()
-            lines = contents.split('\n')
-            do_search(quest, lines)
-
-
-if __name__ == '__main__':
-    main()
+while True:
+    user_query = input("\nSearch word or line: ")
+    do_search(user_query)
