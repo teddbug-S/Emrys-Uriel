ca5c1d993dc677bb4bc9c141e56f73cbe982ea9e
Divine Darkey <teddbug47@outlook.com>
2020-11-24 00:50:16 +0000

    Update setup.py

--- CLI/setup.py
+++ CLI/setup.py
@@ -2,11 +2,12 @@ from setuptools import setup
 
 setup(
     name='CLI',
-    version='0.4.0',
-    packages=['search'],
+    version='0.7.0',
+    packages=['search', 'textspider'],
     entry_points={
         'console_scripts': [
             'search = search.__main__:main',
+            'textspider' = textspider.__main__:main',
         ]
     }
 )
